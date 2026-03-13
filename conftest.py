eafd0a012c51122a33f87b4a62d9544f2675e769
Olga Stepanova <triazed@gmail.com>
2026-03-13 22:17:14 +0500

    Add fixture for product added to cart state

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from config import settings
+from locators.components.header_locators import HeaderLocators
+from locators.search_results_page_locators import SearchResultsPageLocators
+from test_data import products
+from config import urls
 from pages.page_factory import PageFactory
 
 
@@ -26,3 +33,14 @@ def driver(request):
 @pytest.fixture(scope="function")
 def pages(driver):
     return PageFactory(driver)
+
+@pytest.fixture(scope="function")
+def product_added_to_cart(driver):
+    driver.get(urls.BASE_URL)
+    product = products.SEARCHED_PRODUCT_NAME_QTY_1
+    WebDriverWait(driver, settings.DEFAULT_TIMEOUT).until(EC.visibility_of_element_located(HeaderLocators.SEARCH_INPUT)).send_keys(product)
+    WebDriverWait(driver, settings.DEFAULT_TIMEOUT).until(EC.element_to_be_clickable(HeaderLocators.SEARCH_BUTTON)).click()
+    WebDriverWait(driver, settings.DEFAULT_TIMEOUT).until(EC.element_to_be_clickable(SearchResultsPageLocators.searched_product_add_to_cart_button(product))).click()
+    WebDriverWait(driver, settings.DEFAULT_TIMEOUT).until(EC.element_to_be_clickable(HeaderLocators.NOTIFICATION_CLOSE_BUTTON)).click()
+    return product
+
